e8e1575462a1a83d3b7959f20b7ba257688772cc
GhostofSteel <joel.lundhag@gmail.com>
2022-02-11 15:16:36 +0100

    feat: camer controls

--- src/features/levelPlayer/background.tsx
+++ src/features/levelPlayer/background.tsx
@@ -1,15 +1,15 @@
-import { useFrame } from '@react-three/fiber';
-import niceColors from 'nice-color-palettes';
-import { memo, useRef } from 'react';
-import { Color, InstancedMesh, Matrix4 } from 'three';
+import { useFrame } from "@react-three/fiber";
+import niceColors from "nice-color-palettes";
+import { memo, useRef } from "react";
+import { Color, InstancedMesh, Matrix4 } from "three";
 
 const colors = niceColors[1];
 
-const data = new Array(64 * 64).fill(0).map((d, id) => ({
+const data = new Array(256 * 32).fill(0).map((d, id) => ({
   id,
   mat4: new Matrix4().setPosition(
-    (id % 64) * 1.05 - 6.3,
-    Math.floor(id / 64) * 1.05 - 3.3,
+    (id % 256) * 1.05,
+    Math.floor(id / 32) * 0.135,
     0
   ),
   col3: new Color().setHex(
@@ -33,12 +33,12 @@ const Cell = memo<{
     position={[value.x, value.y, 0]}
     onPointerEnter={() => onActive(value.id)}
   >
-    - <boxBufferGeometry attach='geometry' args={[1, 1, 0, 32]} />
-    <meshStandardMaterial attach='material' />
+    - <boxBufferGeometry attach="geometry" args={[1, 1, 0, 32]} />
+    <meshStandardMaterial attach="material" />
   </mesh>
 ));
 
-Cell.displayName = 'Cell';
+Cell.displayName = "Cell";
 
 export const Background = memo(() => {
   const cellMesh = useRef<InstancedMesh>(null);
@@ -49,7 +49,7 @@ export const Background = memo(() => {
       cellMesh.current?.setMatrixAt(i, d.mat4);
 
       if (d.id === active.current) {
-        cellMesh.current?.setColorAt(i, new Color().setColorName('red'));
+        cellMesh.current?.setColorAt(i, new Color().setColorName("red"));
       } else {
         cellMesh.current?.setColorAt(i, d.col3);
       }
@@ -68,10 +68,10 @@ export const Background = memo(() => {
       args={[undefined, undefined, data.length]}
       onPointerEnter={(ev) => (active.current = ev.instanceId ?? -1)}
     >
-      <planeBufferGeometry attach='geometry' />
-      <meshStandardMaterial attach='material' />
+      <planeBufferGeometry attach="geometry" />
+      <meshStandardMaterial attach="material" />
     </instancedMesh>
   );
 });
 
-Background.displayName = 'Background';
+Background.displayName = "Background";
--- src/features/levelPlayer/index.tsx
+++ src/features/levelPlayer/index.tsx
@@ -1,48 +1,75 @@
-import { animated, useSpring } from '@react-spring/three';
-import { Canvas } from '@react-three/fiber';
-import React, { FC, Suspense, useRef, useState } from 'react';
+import { animated, useSpring } from "@react-spring/three";
+import { FlyControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, {
+  FC,
+  Suspense,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
-import { Background } from './background';
-import { GUI } from './gui';
+import { Background } from "./background";
+import { GUI } from "./gui";
 
-const LevelMaker: FC = () => {
+const LevelPlayer: FC = () => {
   const canvas = useRef<HTMLCanvasElement>(null);
 
   const [hovered, setHovered] = useState(false);
 
   const { scale, color } = useSpring({
     scale: hovered ? 1.5 : 1,
-    color: hovered ? 'hotpink' : 'red',
+    color: hovered ? "hotpink" : "red",
+  });
+
+  const escFunction = useCallback((event) => {
+    console.log(event.key);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
   });
 
   return (
-    <section className='m-auto max-w-screen-lg'>
-      <h1 className='text-xl font-bold'>Level Creator</h1>
+    <section className="m-auto max-w-screen-lg">
+      <h1 className="text-xl font-bold">Level Creator</h1>
 
       <Canvas
-        className='aspect-video border-[1px] border-slate-600 max-h-[80vh] m-auto w-full border-solid'
+        className="aspect-video border-[1px] border-slate-600 max-h-[80vh] m-auto w-full border-solid"
         ref={canvas}
       >
-        <Suspense fallback={null}>
-          <Background />
-          <GUI />
-
-          <hemisphereLight position={[10, 0, 0]} />
-
+        <PerspectiveCamera>
           <animated.mesh
             scale={scale}
             onPointerEnter={() => setHovered(true)}
             onPointerLeave={() => setHovered(false)}
+            position={[5, 5, 0]}
           >
             <sphereBufferGeometry />
             {/*
          // @ts-expect-error this works */}
             <animated.meshStandardMaterial color={color} />
           </animated.mesh>
+        </PerspectiveCamera>
+        <Suspense fallback={null}>
+          <Background />
+          <GUI />
+
+          <hemisphereLight position={[0, 0, 0]} />
         </Suspense>
+        <FlyControls
+          autoForward={false}
+          dragToLook={false}
+          movementSpeed={100}
+        />
       </Canvas>
     </section>
   );
 };
 
-export default LevelMaker;
+export default LevelPlayer;
